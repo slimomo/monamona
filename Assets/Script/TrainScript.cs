ad0ecdd2b523f69758add0e24cd9035ddd444e6f
Yuna Takahashi <y.takahashi99624@gmail.com>
2021-02-06 21:22:42 +0900

    RandomImage

--- Assets/Script/TrainScript.cs
+++ Assets/Script/TrainScript.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrainScript : MonoBehaviour
 {
     public GameObject[] Train;
+    public Text ScoreText;
+    public int score = 0;
 
     void Start()
     {
@@ -13,7 +16,53 @@ public class TrainScript : MonoBehaviour
     public void MakeObject()
     {
         int number = Random.Range(0, Train.Length);
+        Train[number].SetActive(true);
         Instantiate(Train[number], transform.position, transform.rotation);
+        
         Debug.Log("生成");
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Train[2].activeSelf || Train[7].activeSelf)
+            {
+                Debug.Log("maruSSS");
+                score += 10;
+                ScoreText.text = "" + score;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            //Debug.Log("N was pressed.");
+            if (Train[0].activeSelf || Train[1].activeSelf)
+            {
+                Debug.Log("maruNNN");
+                score += 10;
+                ScoreText.text = "" + score;
+                Train[0].SetActive(false);
+                Train[1].SetActive(false);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            //Debug.Log("M was pressed.");
+            if (Train[3].activeSelf || Train[4].activeSelf || Train[6].activeSelf || Train[8].activeSelf)
+            {
+                Debug.Log("maruMMM");
+                score += 10;
+                ScoreText.text = "" + score;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            //Debug.Log("N was pressed.");
+            if (Train[5].activeSelf)
+            {
+                Debug.Log("maruJJJ");
+                score += 10;
+                ScoreText.text = "" + score;
+            }
+        }
     }
 }
