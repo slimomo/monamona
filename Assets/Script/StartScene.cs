a979809d2566c7a6f9ceda75680f4868cae4ee93
Yuna Takahashi <y.takahashi99624@gmail.com>
2021-02-06 20:04:46 +0900

    FirstCommit

---
+++ Assets/Script/StartScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public GameObject RuleImage;
+    public GameObject ToziruButton;
+
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void OnClickRuleButton()
+    {
+        RuleImage.SetActive(true);
+        ToziruButton.SetActive(true);
+    }
+    public void OnClickRankingButton()
+    {
+        //SceneManager.LoadScene("GameScene");
+    }
+    public void OnClickToziruButton()
+    {
+        RuleImage.SetActive(false);
+        ToziruButton.SetActive(false);
+    }
+}
---
+++ Assets/Script/TrainScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainScript : MonoBehaviour
+{
+    public GameObject[] Train;
+
+    void Start()
+    {
+        MakeObject();
+    }
+    public void MakeObject()
+    {
+        int number = Random.Range(0, Train.Length);
+        Instantiate(Train[number], transform.position, transform.rotation);
+        Debug.Log("生成");
+    }
+}
